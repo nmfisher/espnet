e25f4f68e1eb028249b3d48bb4c912b05e09b4bd
Nick Fisher <nick.fisher@avinium.com>
2022-01-13 19:22:14 +0800

    update duration cluster extraction

---
+++ egs2/TEMPLATE/asr1/pyscripts/feats/cluster-durations.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import sys
+import numpy as np
+from espnet.transform.transformation import Transformation
+from espnet.utils.cli_readers import file_reader_helper
+from espnet.utils.cli_utils import get_commandline_args
+from espnet.utils.cli_utils import is_scipy_wav_style
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="converts raw phone durations to clustered labels",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("--verbose", "-V", default=0, type=int, help="Verbose option")
+    # parser.add_argument(
+    #     "n_fft",
+    #     type=int,
+    #     default=1024,
+    #     help="n_fft used during feat extraction (this is needed as this script accepts ms durations as input, which are then converted to frames"
+    # )
+    parser.add_argument(
+        "sample_rate",
+        type=int,
+        default=16000,
+        help="hop_length used during feat extraction (this is needed as this script accepts ms durations as input, which are then converted to frames"
+    )
+    parser.add_argument(
+        "hop_length",
+        type=int,
+        default=256,
+        help="hop_length used during feat extraction (this is needed as this script accepts ms durations as input, which are then converted to frames"
+    )
+    parser.add_argument(
+        "num_clusters", type=int, help="Number of clusters to use for durations"
+    )
+    parser.add_argument(
+        "train_durations", type=str, help="Path to train durations file. e.g. data/train/durations"
+    )
+    parser.add_argument(
+        "train_transcripts", type=str, help="Path to train transcripts. e.g. data/train/text"
+    )
+    parser.add_argument(
+        "train_outfile", type=str, help="Output path for train duration cluster IDs data/train/cluster_ids"
+    )
+    parser.add_argument(
+        "valid_durations", type=str, help="Path to validation durations file. e.g. data/valid/durations"
+    )
+    parser.add_argument(
+        "valid_transcripts", type=str, help="Path to valid transcripts. e.g. data/valid/text"
+    )
+    parser.add_argument(
+        "valid_outfile", type=str, help="Output path for validation duration cluster IDs data/train/cluster_ids"
+    )
+    parser.add_argument(
+        "out",
+        nargs="?",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="The output filename. " "If omitted, then output to sys.stdout",
+    )
+    return parser
+
+def _open(path):
+  with open(path, "r") as infile:
+    for line in infile.readlines():
+      split = line.strip().split(" ")
+      yield (split[0], split[1:])
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    # logging info
+    logfmt = "%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s"
+    if args.verbose > 0:
+        logging.basicConfig(level=logging.INFO, format=logfmt)
+    else:
+        logging.basicConfig(level=logging.WARN, format=logfmt)
+    logging.info(get_commandline_args())
+
+    # first pass through the dataset collects the durations for every phone and stores in a map
+    # keys are phones, values are list of durations
+    # e.g. {
+    #  "h":[10,15,...,17,27],
+    #  "e":[20,45,...,57,77],
+    # etc
+    # }
+
+    train_durations = [(x[0], [int((float(d) * args.sample_rate) / args.hop_length) + 1 for d in x[1]]) for x in _open(args.train_durations)]
+    valid_durations = [(x[0], [int((float(d) * args.sample_rate) / args.hop_length) + 1 for d in x[1]]) for x in _open(args.valid_durations)]
+    
+    transcripts = _open(args.train_transcripts)
+    durations_all = {}
+
+    for (utt_id, utt_durations), (utt_id, transcript) in zip(train_durations, transcripts):
+        assert len(transcript) == len(utt_durations)
+        for phone, duration in zip(transcript, utt_durations):
+            if phone not in durations_all:
+                durations_all[phone] = []
+            durations_all[phone].append(duration)
+    
+    durations_mean = { phone:int(np.mean(durations_all[phone])) for phone in durations_all }
+    
+    # sort the mean durations and bucket into [num_clusters] so that each bucket contains an equal number of samples
+    # produces a list of floats 
+    # each float corresponds to the high end of the cluster duration (and each index corresponds to the cluster label)
+    # e.g. {
+    #  "h":[10,15,27],
+    #  "e":[20,45,57], (so cluster 0 <= 20, cluster 1 <= 45, etc)
+    # etc
+    # }
+    average_durations = list(durations_mean.values())
+    
+    average_durations.sort()
+
+    interval_len = int(len(average_durations) / args.num_clusters)
+    
+    clusters = average_durations[::interval_len]
+
+    # finally, iterate over the original dataset against to replace the phone durations with the phone cluster IDs
+    # results are written directly to the output
+    for out, durations in [(args.train_outfile, train_durations), (args.valid_outfile, valid_durations)]:
+      with open(out, "w") as outfile:
+        for (utt_id, utt_durations) in durations:
+            cluster_ids = []
+            for duration in utt_durations:
+                for cluster_id in range(args.num_clusters):
+                    if (cluster_id == 0 and duration <= clusters[cluster_id]) or \
+                        (cluster_id == args.num_clusters - 1 and duration >= clusters[cluster_id]) or \
+                        (duration <= clusters[cluster_id] and duration > clusters[cluster_id-1]):
+                        cluster_ids.append(str(cluster_id))
+                        break
+                    else:
+                      continue
+            outfile.write("{} {}\n".format(utt_id, " ".join(cluster_ids)))
+
+if __name__ == "__main__":
+    main()
---
+++ egs2/TEMPLATE/asr1/scripts/feats/cluster_durations.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Begin configuration section.
+nj=4
+cmd=run.pl
+verbose=0
+filetype=""
+preprocess_conf=""
+# End configuration section.
+
+help_message=$(cat << EOF
+Usage: $0 [options] <train-durations> <train-transcript> <valid-durations> <valid-transcript>  [<log-dir>]
+e.g.: $0 teacher_train_dir/durations data/train/text teacher_valid_dir/durations data/valid/text data/train/log
+assuming data/train/text contains the phonetic transcript like:
+SPKR1_UTT1 h eh l l o
+
+Options:
+  --nj <nj>                                        # number of parallel jobs
+  --cmd (utils/run.pl|utils/queue.pl <queue opts>) # how to run jobs.
+  --verbose <num>                                  # Default: 0
+EOF
+)
+
+echo "$0 $*" 1>&2 # Print the command line for logging
+
+. parse_options.sh || exit 1;
+
+if [ $# -lt 2 ] || [ $# -gt 6 ]; then
+    echo "${help_message}" 1>&2
+    exit 1;
+fi
+
+set -euo pipefail
+
+train_durations=$1
+train_transcript=$2
+train_clusters_out=$3
+valid_durations=$4
+valid_transcript=$5
+valid_clusters_out=$6
+
+data=$(dirname ${train_durations})
+logdir=${data}/log
+mkdir -p ${logdir}
+
+# TODO - put sample rate/hop_len/num clusters etc into config
+nj=1
+${cmd} JOB=1:${nj} ${logdir}/cluster_durations.JOB.log \
+    pyscripts/feats/cluster-durations.py 16000 256 15 --verbose ${verbose}  \
+    ${train_durations} ${train_transcript} ${train_clusters_out} ${valid_durations} ${valid_transcript} ${valid_clusters_out}
+echo "DONE!"
