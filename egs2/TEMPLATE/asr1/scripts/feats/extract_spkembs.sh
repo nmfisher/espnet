987e2583e9a34b238ab795304490fb49f452c747
Nick Fisher <nick.fisher@avinium.com>
2022-07-07 11:25:59 +0800

    update spemb extraction for parallel processing

--- egs2/TEMPLATE/asr1/scripts/feats/extract_spkembs.sh
+++ egs2/TEMPLATE/asr1/scripts/feats/extract_spkembs.sh
@@ -9,10 +9,8 @@ preprocess_conf=""
 # End configuration section.
 
 help_message=$(cat << EOF
-Usage: $0 [options] <train-wav> <train-durations> <train-transcript> <valid-wav> <valid-durations> <valid-transcript> <f0min> <f0max>  [<log-dir>]
-e.g.: $0 data/train/wav.scp teacher_train_dir/durations data/train/text data/test/wav.scp teacher_valid_dir/durations data/valid/text data/train/log
-assuming data/train/text contains the phonetic transcript like:
-SPKR1_UTT1 h eh l l o
+Usage: $0 [options] <wav> 
+e.g.: $0 data/train/wav.scp 
 
 Options:
   --nj <nj>                                        # number of parallel jobs
@@ -25,33 +23,46 @@ echo "$0 $*" 1>&2 # Print the command line for logging
 
 . parse_options.sh || exit 1;
 
-if [ $# -lt 5 ] || [ $# -gt 5 ]; then
+if [ $# -lt 1 ] || [ $# -gt 1 ]; then
     echo "${help_message}" 1>&2
     exit 1;
 fi
 
 set -euo pipefail
 
-train_wav=$1
-test_wav=$2
-train_out=$3
-test_out=$4
-stats=$5
+wav=$1
 
-data=$(dirname ${train_wav})
+data=$(dirname ${wav})
 logdir=${data}/log
 mkdir -p ${logdir}
 
 sr=16000
 hop_length=160
 
-# TODO - put sample rate/hop_len/num clusters etc into config
-nj=1
+split_scps=""
+for n in $(seq ${nj}); do
+    split_scps="${split_scps} ${logdir}/wav.${n}.scp"
+done
+
+utils/split_scp.pl ${wav} ${split_scps} || exit 1;
+
 echo "Extracting speaker embeddings"
-_opts="$train_wav $test_wav $train_out $test_out $stats"
 
-echo "Using spkembs opts $_opts"
+#PYTHONPATH=/home/hydroxide/projects/byol-a 
+${cmd} JOB=1:${nj} ${logdir}/extract_spkembs.JOB.log \
+    python3.7 pyscripts/feats/extract-spkembs.py scp:${logdir}/wav.JOB.scp ark,scp:${logdir}/xvector.JOB.ark,${logdir}/xvector.JOB.scp || exit 1;
 
-PYTHONPATH=/home/hydroxide/projects/byol-a ${cmd} JOB=1:${nj} ${logdir}/extract_spkembs.JOB.log \
-    python3.7 pyscripts/feats/extract-spkembs.py $_opts
+# "ark,scp:${data_feats}${_suf}/${valid_set}/xvector.ark,${data_feats}${_suf}/${valid_set}/xvector.scp"                 
 
+for n in $(seq ${nj}); do
+  cat ${logdir}/xvector.${n}.scp || exit 1;
+done > ${data}/xvector.scp || exit 1
+
+rm -f ${logdir}/xvector.*.scp 
+
+nf=$(wc -l < ${data}/xvector.scp)
+nu=$(wc -l < ${data}/wav.scp)
+if [ ${nf} -ne ${nu} ]; then
+    echo "It seems not all speaker embeddings were successful ($nf != $nu);"
+    echo "consider using utils/fix_data_dir.sh $data"
+fi
